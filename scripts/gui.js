f9705b0e191eccdd251a9c6195d2f033d77ae70e
”Erkin <erkenoof@gmail.com>
2025-02-13 01:31:48 +0500

    Добавлена панель управления GUI

---
+++ scripts/gui.js
@@ -0,0 +1,12 @@
+import GUI from 'https://cdn.jsdelivr.net/npm/lil-gui@0.18.0/dist/lil-gui.esm.js';
+
+const gui = new GUI();
+const params = { text: 'Hello, World!', color: '#FF0000' };
+
+gui.add(params, 'text').onChange(value => {
+    document.querySelector('[my-custom-component]').setAttribute('my-custom-component', 'text', value);
+});
+
+gui.addColor(params, 'color').onChange(value => {
+    document.querySelector('[my-custom-component]').setAttribute('my-custom-component', 'color', value);
+});
